8185f789624af99d29864b90dd7e46ba36b3da5a
Chedly Zouche <48986303+Zedo9@users.noreply.github.com>
2021-01-17 22:38:19 +0100

    Added PHPMailer

--- contact_form_handler.php
+++ contact_form_handler.php
@@ -1,22 +1,48 @@
 <?php
+require 'PHPMailer-6.2.0/src/Exception.php';
+require 'PHPMailer-6.2.0/src/PHPMailer.php';
+require 'PHPMailer-6.2.0/src/SMTP.php';
 
-print_r($_POST);
+use PHPMailer\PHPMailer\PHPMailer;
+
+$mail = new PHPMailer();
+
+//remove this line before pushing to prod
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'academyistqb@gmail.com'; 
+$mail->Password = 'cabinet123456789@';
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-$emailTo = "dechly98@gmail.com";
-$subject = "[Formulaire de contact - Nouveau message]";
-$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+$mail->addReplyTo($email, $name); 
+$mail->setFrom('academyistqb@gmail.com', 'ISTQB Academy');
+$mail->addAddress('academyistqb@gmail.com', 'ISTQB Academy');
 
-$email_message = "Form details below.\n\n";
-$email_message .= "Name: " . $name . "\n";
-$email_message .= "Email: " . $email . "\n";
-$email_message .= "Message: " . $message . "\n";
-if (mail($emailTo, $subject, $email_message, $mailHeaders)){
-    print_r("Success");
-}
-else {
-    print_r("error");
+
+$mail->Subject = '[Formulaire de contact - Landing Page]';
+$mail->isHTML(true);
+
+$mailContent = "<h1>Détails du message</h1>";
+$mailContent .= "<h2>Nom: " . $name . "</h2>";
+$mailContent .= "<h2>Email: " . $email . "</h2>";
+$mailContent .= "<h3>Message:" . "</h3>";
+$mailContent .= "<p>" . $message . "</p>";
+    
+$mail->Body = $mailContent;
+
+if($mail->send()){
+    echo "Message sent";
+    /*header("Location: /");
+    exit;*/
+}else{
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
 }
 ?>
